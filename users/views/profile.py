3b86c05aee032d20813ed3693b1f56fa1b836d3e
Zena <majdoulinabdalkareem@gmail.com>
2026-07-10 23:38:31 +0300

    message

--- users/views/profile.py
+++ users/views/profile.py
@@ -4,30 +4,63 @@ from rest_framework.response import Response
 from rest_framework import status
 from drf_spectacular.utils import extend_schema
 
-"""
-@extend_schema(
-    tags=['البروفايل'],
-    summary="جلب أو تحديث بيانات المستخدم الحالي",
-    responses={200: ProfileSerializer}
-)
-@api_view(['GET', 'PUT', 'PATCH'])
-@permission_classes([IsAuthenticated])
-def profile(request):
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
 
-    user_profile, created = Profile.objects.get_or_create(user=request.user)
-    user_profile, created = Profile.objects.get_or_create(user=request.user)
+from users.errors.messages.success import success_response
+from users.serializers.user import UpdateProfileSerializer
 
-    if request.method == 'GET':
-        serializer = ProfileSerializer(user_profile)
-        return Response(serializer.data)
 
-    if request.method in ['PUT', 'PATCH']:
-        serializer = ProfileSerializer(user_profile, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({
-                "message": "updated successfully",
-                "data": serializer.data
-            })
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-"""
+class ProfileView(generics.RetrieveUpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UpdateProfileSerializer
+    parser_classes = [MultiPartParser, FormParser]
+
+    def get_object(self):
+        return self.request.user
+
+    def retrieve(self, request, *args, **kwargs):
+        user = self.get_object()
+        serializer = self.get_serializer(user)
+
+        data = serializer.data
+        data["username"] = user.username
+        data["email"] = user.email
+        data["is_profile_completed"] = bool(user.avatar and user.phone)
+
+        return Response(
+            success_response(
+                message="Profile retrieved successfully",
+                code="PROFILE_RETRIEVED",
+                data=data,
+            ),
+            status=status.HTTP_200_OK,
+        )
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop("partial", False)
+        user = self.get_object()
+
+        serializer = self.get_serializer(
+            user,
+            data=request.data,
+            partial=partial,
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        data = serializer.data
+        data["username"] = user.username
+        data["email"] = user.email
+        data["is_profile_completed"] = bool(user.avatar and user.phone)
+
+        return Response(
+            success_response(
+                message="Profile updated successfully",
+                code="PROFILE_UPDATED",
+                data=data,
+            ),
+            status=status.HTTP_200_OK,
+        )
